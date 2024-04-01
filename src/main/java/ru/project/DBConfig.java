0a91dad2d33f42da4cef2c082186b9bf3ad16016
Pavel <162188163+malipv@users.noreply.github.com>
2024-04-01 16:24:54 +0700

    Commit

--- src/main/java/ru/project/DBConfig.java
+++ src/main/java/ru/project/DBConfig.java
@@ -1,38 +1,16 @@
 package ru.project;
 
 import org.h2.jdbcx.JdbcDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJdbcRepositories(basePackages = "ru.project")
-public class DBConfig extends AbstractJdbcConfiguration {
+public class DBConfig {
     @Bean
     public DataSource dataSource() {
         JdbcDataSource ds = new JdbcDataSource();
         ds.setURL("jdbc:h2:D:\\path\\");
         return ds;
-    }
-    @Bean
-    @Autowired
-    public PlatformTransactionManager transactionManager(DataSource ds) {
-        return new DataSourceTransactionManager(ds);
-    }
-
-    @Bean
-    @Autowired
-    public NamedParameterJdbcOperations namedParameter(DataSource ds) {
-        return new NamedParameterJdbcTemplate(ds);
     }
 }
